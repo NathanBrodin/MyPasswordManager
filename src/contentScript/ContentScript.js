71cb209eb44699c0f234176b2b4554e300001549
Nathan <nathan.brodin07@gmail.com>
2022-08-30 19:34:04 +0300

    Update

--- src/contentScript/ContentScript.js
+++ src/contentScript/ContentScript.js
@@ -1,9 +1,126 @@
 /* global chrome */
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { db } from '../firebase'
 import { doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
 import './ContentScript.css'
 
+export default class ContentScript extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      form: this.getForm(),
+      inputs: this.getInputs(),
+      url: this.getUrl(),
+      user: this.getUser()
+    }
+
+    this.searchUserData()
+  }
+
+  getForm() {
+    const form = document.getElementsByTagName('form')[0]
+    if(form) {
+      return form
+    }
+  }
+
+  getInputs() {
+    var inputs = document.getElementsByTagName('input')
+    inputs = Object.values(inputs).filter(this.filterInputs)
+
+    if(inputs.length) {
+      return inputs
+    }
+  }
+  
+  filterInputs(input) {
+    switch (input.type) {
+      case "email":
+      case "password":
+      case "text":
+      case "tel":    
+        return true
+    
+      default:
+        return false
+    }
+
+  }
+
+  getUrl() {
+    let url = (new URL(window.location.href))
+    url = url.hostname.replace('www.', '')
+
+    if(url) {
+      return url
+    }
+  }
+
+  async getUser() {
+    const user = await chrome.storage.sync.get('user')
+    if(user) {
+      return user
+    }
+  }
+
+  async searchUserData() {
+    if(!this.state.user) { return }
+
+    console.log("All the conditions are met, searching for user data")
+    console.log("User: " + this.state.user, "Url: " + this.state.url, "Form: " + this.state.form, "Inputs: " + this.state.inputs)
+
+    const urlRef = doc(db, "users/" + this.state.user + "/data/" + this.state.url)
+    const urlSnap = await getDoc(urlRef)
+    
+    // if url is stored, get values
+    if(urlSnap.exists()) {
+      console.log("Data stored")
+      this.fillInputs(urlSnap.data())
+
+    } else {
+      console.log("Data not stored")
+      this.state.form.addEventListener('submit', this.storeInputs)
+    }
+  }
+
+  async storeInputs() {
+    console.log("Storing the inputs")
+    const inputs = this.state.inputs
+
+    const urlDoc = doc(db, "users", this.state.user, "data", this.state.url)
+    await setDoc(urlDoc, {})
+
+    for(let input of inputs) {    
+      await updateDoc(urlDoc, {
+        [input.id]: input.value
+      })
+    }
+  }
+
+  fillInputs(userData) {
+    const inputs = this.state.inputs
+
+    for(let input of inputs) {
+      try {
+        input.value = userData[input.id]
+      } catch(e) {
+        console.log("Data of input [" + input.id + "] was not found, " + e)
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="extension-container">
+        <h1>Content Script</h1>
+        <p>This is the content script</p>
+      </div>
+    )
+  }
+}
+
+/*
 export default function ContentScript() {
   const [isStored, setStored] = useState()
 
@@ -53,6 +170,7 @@ export default function ContentScript() {
   async function userData() {
     console.log("All the conditions are met, searching for user data")
 
+    console.log("Current user: " + currentUser, "Url: " + url, "Form: " + form)
     const urlRef = doc(db, "users/" + currentUser + "/data/" + url)
     const urlSnap = await getDoc(urlRef)
     
@@ -148,5 +266,6 @@ export default function ContentScript() {
     <div></div>
   )
 }
+*/
   
 // document.forms[0].submit();
