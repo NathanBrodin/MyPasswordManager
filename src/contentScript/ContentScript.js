153c4a355d08565d73d26153e4bfe1f9215e17b2
Nathan <nathan.brodin07@gmail.com>
2022-09-02 10:18:39 +0300

    Update

--- src/contentScript/ContentScript.js
+++ src/contentScript/ContentScript.js
@@ -16,6 +16,7 @@ export default class ContentScript extends React.Component {
       isStored: true
     }
     this.storeInputs = this.storeInputs.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
 
     console.log(this.state.form)
     if(!this.state.inputs) { return }
@@ -23,14 +24,11 @@ export default class ContentScript extends React.Component {
     this.searchUserData()
   }
 
-  componentDidMount() {
+  handleSubmit() {
     if(!this.state.form && this.state.isStored) { return }
 
-    this.state.form.addEventListener('submit', (e) => { 
-      e.preventDefault()
-      console.log("Form as been submitted")
-      this.storeInputs()
-    })
+    console.log("Submitting the form")
+    this.storeInputs()
   }
 
   getForm() {
@@ -74,6 +72,11 @@ export default class ContentScript extends React.Component {
 
   getUser() {
     return "2SQCgGVyg4Wvyfk39TMDglJqbNd2"
+    /*
+    chrome.identity.getProfileUserInfo(function(userInfo) {
+         this.setState({ user: userInfo.id })
+     });
+     */
   }
 
   async searchUserData() {
@@ -127,164 +130,25 @@ export default class ContentScript extends React.Component {
         console.log("Data of input [" + input.id + "] was not found, " + e)
       }
     }
+
+    //this.state.form.submit()
   }
 
   render() {
     return (
-      <div className="extension-container">
-        <h1>Content Script</h1>
-        <p>This is the content script</p>
+      <div className='extension-container'>
+        <button onClick={this.handleSubmit}>
+          <div className="svg-wrapper-1">
+            <div className="svg-wrapper">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+              </svg>
+            </div>
+          </div>
+          <span>Save</span>
+        </button>
       </div>
     )
   }
 }
-
-/*
-export default function ContentScript() {
-  const [isStored, setStored] = useState()
-
-  const [inputs, setInputs] = useState()
-  const [form, setForm] = useState()
-  const [url, setUrl] = useState()
-  const [currentUser, setCurrentUser] = useState()
-  const [data, setData] = useState()
-
-  useEffect(() => {
-    console.log("ContentScript is running")
-    constructor()
-    // eslint-disable-next-line
-  }, [])
-
-  useEffect(() => {
-    if(isStored) {
-      fillInputs()
-    }
-
-    if(!isStored && form) {
-      console.log("Waiting for user to send data")
-
-      form.addEventListener('submit', function(e) {
-        console.log("Form submitted")
-        storeInputs()
-        setStored(true)
-      })
-
-      return () => {
-        form.removeEventListener('submit', function(e) {})
-      }
-    }
-    // eslint-disable-next-line
-  }, [isStored])
-
-  const constructor = function() {
-
-    setInputs(getInputs())
-    setForm(getForm())
-    setUrl(getUrl())
-
-    getCurrentUser()
-  }
-
-  // Search the data of the user
-  async function userData() {
-    console.log("All the conditions are met, searching for user data")
-
-    console.log("Current user: " + currentUser, "Url: " + url, "Form: " + form)
-    const urlRef = doc(db, "users/" + currentUser + "/data/" + url)
-    const urlSnap = await getDoc(urlRef)
-    
-    // if url is stored, get values
-    if(urlSnap.exists()) {
-      console.log("Data stored")
-
-      setData(urlSnap.data())
-      setStored(true)
-
-    } else {
-      console.log("Data not stored")
-      setStored(false)
-    }
-  }
-  
-  function getInputs() {
-    const inputs = document.getElementsByTagName('input')
-    return Object.values(inputs).filter(filterInputs)
-  }
-  
-  function filterInputs(input) {
-    switch (input.type) {
-      case "email":
-      case "password":
-      case "text":
-      case "tel":    
-        return true
-    
-      default:
-        return false
-    }
-
-  }
-
-  function getForm() {
-    return document.getElementsByTagName('form')[0]
-  }
-  
-  function getUrl() {
-    let url = window.location.href
-    let domain = (new URL(url))
-
-    return domain.hostname.replace('www.', '')
-  }
-
-  async function getCurrentUser() {
-
-    var promise = new Promise(function(resolve, reject) {
-      chrome.storage.sync.get(['user'], function(result) {
-        if(result.user) {
-          resolve(result.user)
-        } else {
-          reject("No user found")
-        }
-      })
-    })
-
-    promise.then(function(result) {
-      console.log("User found: ", result)
-      setCurrentUser(result)
-
-      userData()  // Start searching for user data
-    }, function(err) {
-      console.log(err); // Error: "It broke"
-    });
-  }
-
-  function fillInputs() {
-    for(let input of inputs) {
-      try {
-        input.value = data[input.id]
-      } catch(e) {
-        console.log("Data of input [" + input.id + "] was not found, " + e)
-      }
-    }
-  }
-  
-  async function storeInputs() {
-    console.log("Storing the inputs")
-
-    const urlDoc = doc(db, "users", currentUser, "data", url)
-    await setDoc(urlDoc, {})
-
-    for(let input of inputs) {    
-      await updateDoc(urlDoc, {
-        [input.id]: input.value
-      })
-    }
-  }
-  
-  return (
-    <div></div>
-  )
-}
-*/
-  
-// document.forms[0].submit();
